ed7205f4de94d9d9cfa1639df7d906cadda72a87
ragnarlodbrok1992 <maraus92@gmail.com>
2024-02-12 22:06:07 +0100

    WIP

--- concurrent-spatial-simulation.go
+++ concurrent-spatial-simulation.go
@@ -2,8 +2,44 @@ package main
 
 import (
   "fmt"
+  raylib "github.com/gen2brain/raylib-go/raylib"
 )
 
+const WINDOW_WIDTH int32 = 1280
+const WINDOW_HEIGHT int32 = 720
+const FONT_NAME string = "assets/fonts/SourceCodePro-Medium.ttf"
+const DEFAULT_FONT_SIZE float32 = 20
+const DEFAULT_SPACING float32 = 2
+
+func drawTextDefault(font raylib.Font, text string, x float32, y float32) {
+  raylib.DrawTextEx(font, text, raylib.Vector2{x, y}, DEFAULT_FONT_SIZE, DEFAULT_SPACING, raylib.DarkGray)
+}
+
 func main() {
-  fmt.Println("Concurrent Real-Time Spatial Simulation System - early alpha version.")
+  // Start message
+  fmt.Println("Concurrent Spatial Simulation System - early alpha version.")
+  defer fmt.Println("All went well, goodbye...")
+
+  raylib.InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Concurrent Spatial Simulation System")
+  defer raylib.CloseWindow()
+
+  raylib.SetTargetFPS(60)
+
+  // Loading different font
+  DEFAULT_FONT := raylib.LoadFont(FONT_NAME)
+
+  for !raylib.WindowShouldClose() {
+    // Input handling
+
+    // Simulation
+
+    // Frame drawing
+    raylib.BeginDrawing()
+
+    raylib.ClearBackground(raylib.RayWhite)
+    drawTextDefault(DEFAULT_FONT, "Concurrent Spatial Simulation System", 10, 10)
+
+    raylib.EndDrawing()
+  }
+
 }
--- go.mod
+++ go.mod
@@ -2,3 +2,8 @@ module github.com/ragnarlodbrok1992/concurrent-spatial-simulation
 
 go 1.21
 
+require (
+	github.com/ebitengine/purego v0.6.0 // indirect
+	github.com/gen2brain/raylib-go/raylib v0.0.0-20240125111008-83d871a38f28 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+)
